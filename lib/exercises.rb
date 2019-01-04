674fc23e4d4cc4c0521c2878f518239f9a9a820e
Steven Clontz <steven.clontz@gmail.com>
2019-01-04 22:14:53 +0000

    import mastr build using seeds.json

--- lib/exercises.rb
+++ lib/exercises.rb
@@ -5,10 +5,12 @@ class ExercisesDataSource < Nanoc::DataSource
     items = []
     Dir.foreach('../mastr/build/diff-eq') do |standard|
       next if ['.','..'].include? standard
-      content = ""
-      Dir.foreach("../mastr/build/diff-eq/#{standard}") do |seed|
-        next unless seed[-5..-1]==".html"
-        content << File.read("../mastr/build/diff-eq/#{standard}/#{seed}")
+      seeds = JSON.parse(File.read "../mastr/build/diff-eq/#{standard}/seeds.json")
+      content = "<h2>#{seeds["title"]}</h2>\n"
+      seed_ints = seeds["seeds"].map{|seed| seed["_seed"]}.sort
+      seed_ints.each do |seed_int|
+        content << "<h4>Example #{seed_int}</h4>"
+        content << File.read("../mastr/build/diff-eq/#{standard}/#{seed_int.to_s.rjust(3, "0")}.html")
       end 
       items << new_item(
         content,
