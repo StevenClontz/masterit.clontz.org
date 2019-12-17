302d1c31f7a37e7ca0a2415a905eb34f5984a710
Steven Clontz <steven.clontz@gmail.com>
2019-12-17 14:38:49 -0600

    support diff eq and linear alg

--- lib/exercises.rb
+++ lib/exercises.rb
@@ -3,20 +3,24 @@ class ExercisesDataSource < Nanoc::DataSource
 
   def items
     items = []
-    Dir.foreach('../mastr/build/diff-eq') do |standard|
-      next if ['.','..'].include? standard
-      seeds = JSON.parse(File.read "../mastr/build/diff-eq/#{standard}/seeds.json")
-      content = "<h2>#{seeds["title"]}</h2>\n"
-      seed_ints = seeds["seeds"].map{|seed| seed["_seed"]}.sort
-      seed_ints.each do |seed_int|
-        content << "<h4>Example #{seed_int}</h4>"
-        content << File.read("../mastr/build/diff-eq/#{standard}/#{seed_int.to_s.rjust(3, "0")}.html")
-      end 
-      items << new_item(
-        content,
-        {title: "Diff EQ #{standard}"},
-        Nanoc::Identifier.new("/diff-eq/#{standard}.html"),
-      )
+    courses = ['diff-eq', 'lin-alg']
+    courses.each do |course|
+      Dir.foreach("../mastr/build/#{course}") do |standard|
+        next if ['.','..'].include? standard
+        seeds = JSON.parse(File.read "../mastr/build/#{course}/#{standard}/seeds.json")
+        content = "<h2>#{seeds["title"]}</h2>\n"
+        seed_ints = seeds["seeds"].map{|seed| seed["_seed"]}.sort
+        seed_ints.each do |seed_int|
+          next if seed_int > 50
+          content << "<h4>Example #{seed_int}</h4>"
+          content << File.read("../mastr/build/#{course}/#{standard}/#{seed_int.to_s.rjust(3, "0")}.html")
+        end 
+        items << new_item(
+          content,
+          {title: "#{course.upcase} #{standard}"},
+          Nanoc::Identifier.new("/#{course}/#{standard}.html"),
+        )
+      end
     end
 
     return items
