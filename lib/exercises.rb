8c7b0eee31e51bf50150f5831c2d3e8b47d2fd7a
Steven Clontz <steven.clontz@gmail.com>
2020-01-11 21:41:01 +0000

    refactor styling to Bootstrap4

--- lib/exercises.rb
+++ lib/exercises.rb
@@ -7,9 +7,11 @@ class ExercisesDataSource < Nanoc::DataSource
       next if standard[0]=="."
       seeds = JSON.parse(File.read "../tbil-la/exercises/build/#{standard}/seeds.json")
       content = "<h2>#{seeds["title"]}</h2>\n"
+      content << "<hr/>\n"
       (1..50).each do |seed_int|
         content << "<h4>Example #{seed_int}</h4>"
         content << File.read("../tbil-la/exercises/build/#{standard}/#{seed_int.to_s.rjust(3, "0")}.html")
+        content << "<hr/>\n"
       end 
       items << new_item(
         content,
@@ -21,9 +23,11 @@ class ExercisesDataSource < Nanoc::DataSource
       next if standard[0]=="."
       seeds = JSON.parse(File.read "../mastr-diff-eq/build/#{standard}/seeds.json")
       content = "<h2>#{seeds["title"]}</h2>\n"
+      content << "<hr/>\n"
       (1..50).each do |seed_int|
         content << "<h4>Example #{seed_int}</h4>"
         content << File.read("../mastr-diff-eq/build/#{standard}/#{seed_int.to_s.rjust(3, "0")}.html")
+        content << "<hr/>\n"
       end 
       items << new_item(
         content,
